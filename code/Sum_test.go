3e2572b7231c041d1bb67b20ca52ad85389f784b
mario <mario.occhinegro@abusix.com>
2022-10-22 10:17:29 +0200

    changed tests

--- code/Sum_test.go
+++ code/Sum_test.go
@@ -3,6 +3,9 @@ import (
 
 	"testing"
 	"testing/quick"
+	"github.com/leanovate/gopter"
+	"github.com/leanovate/gopter/gen"
+	"github.com/leanovate/gopter/prop"
 )
 //naming: sth_test.go
 //First word is test
@@ -15,15 +18,52 @@ func TestSum(t *testing.T){
 		//t.Log can provide debug info
 		t.Errorf("Sum was incorrect, got: %d, want: %d", total, 10)
 	}
-	//Property based testing
-	//property
+	//============
+	//quick
+	//============
+	//properties
 	p := func(a int, b int) bool {
 		//summe muss immer größer sein
 		return Sum(a,b) >= a || Sum(a,b) >= b
 	}
+	//identity element property
+	identityElementProperty := func(a int) bool{
+		left := Sum(a, 0)
+		right:= Sum(0,a)
+		return left == a && right == a && left == right
+	}
 	//use property to test
+	//hopefully fails due to overflow
 	err:= quick.Check(p, nil);
 	if err != nil {
 		t.Error(err)
 	}
+	//passing property
+	errIdentity := quick.Check(identityElementProperty, nil);
+	if errIdentity != nil {
+		t.Error(err)
+	}
+	//============
+	//gopter
+	//============
+	parameters := gopter.DefaultTestParameters()
+	properties := gopter.NewProperties(parameters)
+	properties.Property("summe greater than individual values", prop.ForAll(
+		func(v float64) bool {
+			return math.Sqrt(v) >= 1
+		},
+		gen.Float64().SuchThat(func(x float64) bool { return x >= 1.0 }),
+	))
+
+	properties.Property("identity", prop.ForAll(
+		func(v float64) bool {
+			r := math.Sqrt(v)
+			return math.Abs(r*r-v) < 1e-10*v
+		},
+		gen.Float64().SuchThat(func(x float64) bool { return x >= 0.0 }),
+	))
+	properties.TestingRun(t)
+	//============
+	//rapid
+	//============
 }
