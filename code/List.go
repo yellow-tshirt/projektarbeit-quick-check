02800960f85e9020627eb5868c923e53eac1a4a9
mario <mario.occhinegro@abusix.com>
2022-10-30 17:31:26 +0100

    implemented list, insert, reverse, pop and display

---
+++ code/List.go
@@ -0,0 +1,84 @@
+package main
+
+import (
+
+	"fmt"
+	"errors"
+)
+
+type Node struct {
+	next *Node
+	value int
+}
+
+type List struct {
+	head *Node
+}
+
+func (L *List) Insert(value int){
+	var newNode *Node = &Node{value: value}
+	var currentHead *Node = L.head
+	if L.head == nil {
+		L.head = newNode
+	} else {
+		newNode.next = currentHead;
+		L.head = newNode
+	}
+}
+
+func (L *List) Reverse() {
+	var currentNode *Node = L.head
+	var newList List = List{}
+	for{
+
+		if(currentNode == nil){
+			break
+		}
+		value, err:= L.Pop()
+		if(err != nil){
+			fmt.Println(err)
+		}
+		newList.Insert(value)
+		currentNode = currentNode.next
+	}
+	L.head = newList.head
+}
+
+
+func (L *List) Pop() (int, error) {
+	if(L.head != nil){
+		var currentHead *Node = L.head
+		L.head = L.head.next
+		return currentHead.value, nil
+	}
+	return -1, errors.New("cannot pop from empty list")
+}
+
+
+func (l *List) Display() {
+	list := l.head
+	for list != nil {
+		if(list.next == nil){
+		fmt.Printf("%+v", list.value)
+		}else{
+		fmt.Printf("%+v, ", list.value)
+		}
+		list = list.next
+	}
+	fmt.Println()
+}
+
+
+func main() {
+	fmt.Println("===Lists===")
+	var myList List = List{}
+	myList.Insert(1)
+	myList.Insert(2)
+	myList.Insert(3)
+	myList.Insert(4)
+	myList.Insert(5)
+	myList.Insert(6)
+	myList.Pop()
+	myList.Reverse()
+	myList.Display()
+}
---
+++ code/list_test.go
@@ -0,0 +1,12 @@
+package main
+
+import (
+	"testing"
+)
+
+func Test(t *testing.T) {
+	total := 5+5
+	if total != 10 {
+		t.Errorf("Sum was incorrect, got: %d, want: %d", total, 10)
+	}
+}
