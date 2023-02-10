a1d4397025f849ff6f377c7dc2cf97aa4efa3080
mario <mario.occhinegro@abusix.com>
2023-02-10 13:38:37 +0100

    added listSize and popFirst

--- haskell/p.hs
+++ haskell/p.hs
@@ -11,6 +11,14 @@ reverseList :: [Int] -> [Int]
 reverseList [] = []
 reverseList (x:xs) = reverseList xs ++ [x]
 
+listSize :: [Int] -> Int
+listSize [] = 0
+listSize (x:xs) = 1 + listSize xs
+
+popFirst :: [Int] -> [Int]
+popFirst [] = []
+popFirst (x:xs) = xs
+
 main = do
     let numbers = [1,2,3,4,5]
     let newNumbers = reverseList numbers
